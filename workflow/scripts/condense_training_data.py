0f0f4c1a8c2780137d7d933ce5c59008b2f70576
austintdaigle <austin.t.daigle@gmail.com>
2024-02-15 11:24:34 -0500

    Formatting to please the linter

--- workflow/scripts/condense_training_data.py
+++ workflow/scripts/condense_training_data.py
@@ -4,12 +4,15 @@ from tqdm import tqdm
 import os
 import argparse
 
+
 def load_npy(npfile):
     return np.load(npfile)
+
 
 def extract_name(filename):
     bn = os.path.basename(filename)
     return bn.split(".npy")[0]
+
 
 def parse_args():
     parser = argparse.ArgumentParser()
@@ -19,7 +22,7 @@ def parse_args():
         metavar="INDIR",
         required=True,
         dest="indir",
-        help="Top-level directory containing subdirs/npy files to condense."
+        help="Top-level directory containing subdirs/npy files to condense.",
     )
     parser.add_argument(
         "-o",
@@ -27,7 +30,7 @@ def parse_args():
         metavar="OUTFILE",
         required=True,
         dest="outfile",
-        help="NPZ file to write condensed data to."
+        help="NPZ file to write condensed data to.",
     )
 
     return parser.parse_args()
@@ -35,12 +38,15 @@ def parse_args():
 
 def main():
     ap = parse_args()
-         
+
     data_dict = {}
-    for nfile in tqdm(glob(f"{ap.indir}/**/*.npy", recursive=True), desc="Condensing data"):
+    for nfile in tqdm(
+        glob(f"{ap.indir}/**/*.npy", recursive=True), desc="Condensing data"
+    ):
         data_dict[extract_name(nfile)] = load_npy(nfile)
-        
+
     np.savez(ap.outfile, **data_dict)
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
